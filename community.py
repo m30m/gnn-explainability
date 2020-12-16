25df8dff4e92a8acfb64a4c7aa7f2c1ebb1c50d6
MohammadAmin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2020-12-16 08:57:34 +0100

    Change community dataset
    Remove the dummy concept and just rewire inter-community edges to
    intra-community and see if attribution value increase

--- community.py
+++ community.py
@@ -1,10 +1,11 @@
 import random
+from collections import defaultdict
 
 import networkx as nx
 import numpy as np
 import torch
 from torch_geometric.data import Data
-from torch_geometric.utils import k_hop_subgraph
+from torch_geometric.utils import k_hop_subgraph, from_networkx
 import mlflow
 from tqdm import tqdm
 
@@ -23,6 +24,11 @@ def make_coo_edges_by_id(g, N, rewired_neighbors):
            torch.tensor(edges_rewired, dtype=torch.long).t().contiguous()
 
 
+# dummy class for protecting these attributes from batch collation or movement to CUDA
+class Rewiring:
+    pass
+
+
 def make_data():
     K = 10
     SZ = 100
@@ -30,13 +36,13 @@ def make_data():
     P = 0.15
     Q = 0.04
     PERTURB_COUNT = 300
-    DUMMY_COUNT = 500
+    REWIRE_COUNT = 50
     mlflow.log_param('P', P)
     mlflow.log_param('Q', Q)
     mlflow.log_param('N', N)
     mlflow.log_param('K', K)
     mlflow.log_param('PERTURB_COUNT', PERTURB_COUNT)
-    mlflow.log_param('DUMMY_COUNT', DUMMY_COUNT)
+    mlflow.log_param('REWIRE_COUNT', REWIRE_COUNT)
     sizes = [SZ] * K
 
     probs = np.ones((K, K)) * Q
@@ -45,56 +51,64 @@ def make_data():
 
     colors = []
     for i in range(K):
-        colors.extend([i + 1] * SZ)
-    colors.extend([0] * (DUMMY_COUNT))  # color of dummy nodes
+        colors.extend([i] * SZ)
     labels = colors.copy()
 
-    for i in random.sample(list(range(N)), PERTURB_COUNT):
-        choices = list(range(1, 11))
+    for i in random.sample(list(range(N)), 500):
+        choices = list(range(10))
         # choices.remove(colors[i])
         colors[i] = random.choice(choices)
         # colors[i]=0
 
-    rewired_neighbors = {}
-    dummy_node_i = 0
-    for i in random.sample(list(range(N)), DUMMY_COUNT):
-        g.add_edge(i, dummy_node_i + N)
-        dummy_node_i += 1
-        rewired_neighbor = i
-        while rewired_neighbor == i:
-            start_i = i - (i % 100)
-            rewired_neighbor = random.randint(start_i, start_i + 99)
-        rewired_neighbors[i] = rewired_neighbor
-    dummy_mask = torch.zeros(len(g.nodes()), dtype=bool)
-    dummy_mask[N:] = True
-    g = g.to_directed()
-    dummy_edges = torch.zeros(len(g.edges()), dtype=bool)
-    eid = 0
-    for u, v, d in g.edges(data=True):
-        d['id'] = eid
-        if u >= N or v >= N:
-            dummy_edges[eid] = True
-        eid += 1
-    edge_index, edge_index_rewired = make_coo_edges_by_id(g, N, rewired_neighbors)
-
-    features = np.zeros((len(g.nodes()), 11))
-
+    features = np.zeros((len(g.nodes()), 10))
     for i, c in enumerate(colors):
         features[i, c] = 1
 
-    data = Data(x=torch.tensor(features, dtype=torch.float),
-                edge_index=edge_index,
-                y=torch.tensor(labels))
-    data.dummy_mask = dummy_mask
-    data.dummy_edges = dummy_edges
+    data = from_networkx(g)
+    data.x = torch.tensor(features, dtype=torch.float)
+    data.y = torch.tensor(labels)
+
+    edge_to_id = {}
+    id_to_edge = {}
+    bad_edges = defaultdict(list)
+    for eid in range(data.num_edges):
+        u, v = data.edge_index[:, eid]
+        u, v = u.item(), v.item()
+        edge_to_id[(u, v)] = eid
+        id_to_edge[eid] = (u, v)
+        if labels[u] != labels[v]:
+            bad_edges[labels[u]].append((u, v))
+
+    node_edits = {}
+    for i in g.nodes():
+        rewires = random.sample(bad_edges[labels[i]], REWIRE_COUNT)
+        nodes_with_same_label = [x for x in g.nodes() if labels[x] == labels[i]]
+        new_edges = set()
+        edits = {}
+        for u, v in rewires:
+            assert labels[u] == labels[i]
+            v2 = random.choice(nodes_with_same_label)
+            while v2 == u or (u, v2) in edge_to_id or (u, v2) in new_edges:
+                v2 = random.choice(nodes_with_same_label)
+
+            edits[edge_to_id[(u, v)]] = (u, v2)
+            edits[edge_to_id[(v, u)]] = (v2, u)
+            new_edges.add((u, v2))
+        assert len(edits) == 100
+        node_edits[i] = edits
+
+    rewiring = Rewiring()
+    rewiring.id_to_edge = id_to_edge
+    rewiring.edge_to_id = edge_to_id
+    rewiring.node_edits = node_edits
+    data.rewiring = rewiring
     data.num_classes = len(set(labels))
-    data.edge_index_rewired = edge_index_rewired
 
     return data
 
-
 def evaluate_explanation(explain_function, model, test_dataset):
     accs = []
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     for dss in test_dataset:
         bads = 0
         before_afters = []
@@ -102,31 +116,38 @@ def evaluate_explanation(explain_function, model, test_dataset):
         tests = 0
         pbar = tqdm(range(1000))
         model_cache = model(dss.x, dss.edge_index)
-        model_cache_rewired = model(dss.x, dss.edge_index_rewired)
+        edge_index_rewired = dss.edge_index.clone().to(device)
+        rewire_mask = torch.zeros(dss.num_edges, dtype=bool)
         for node_idx in pbar:
             prob, label = model_cache[[node_idx]].softmax(dim=1).max(dim=1)
-            prob_rewired, label_rewired = model_cache_rewired[[node_idx]].softmax(dim=1).max(dim=1)
+            for eid, (u, v) in dss.rewiring.node_edits[node_idx].items():
+                edge_index_rewired[:, eid] = torch.tensor([u, v], dtype=int)
+                rewire_mask[eid] = True
+
+            prob_rewired, label_rewired = model(dss.x, edge_index_rewired)[[node_idx]].softmax(dim=1).max(dim=1)
             target = dss.y[node_idx].item()
             if label_rewired.item() == target and prob_rewired.item() > prob.item():
-                pass
-            else:
-                # print(f'not true for node_idx {node_idx} first label: {label.item()} rewired label: {label_rewired.item()} diff {prob_rewired.item()-prob.item()}')
-                continue
-            final_mask = (k_hop_subgraph(node_idx, depth_limit, dss.edge_index)[3] &
-                          k_hop_subgraph(node_idx, depth_limit, dss.edge_index_rewired)[3] &
-                          dss.dummy_edges)
-            final_mask = final_mask
+                final_mask = (k_hop_subgraph(node_idx, depth_limit, dss.edge_index)[3] &
+                              k_hop_subgraph(node_idx, depth_limit, edge_index_rewired)[3])
 
-            attribution = explain_function(model, node_idx, dss.x, dss.edge_index, target)[final_mask.cpu()]
-            attribution_rewired = explain_function(model, node_idx, dss.x, dss.edge_index_rewired, target)[final_mask.cpu()]
+                final_mask = final_mask.cpu() & rewire_mask
+                # for other explanations
+                attribution = explain_function(model, node_idx, dss, dss.edge_index)[final_mask]
+                attribution_rewired = explain_function(model, node_idx, dss, edge_index_rewired)[final_mask]
 
-            before_afters.append((attribution.mean(), attribution_rewired.mean()))
-            if attribution.mean() > attribution_rewired.mean():
-                # print(attribution.mean() , attribution_rewired.mean())
-                bads += 1
-                # print(node_idx)
-            tests += 1
-            pbar.set_postfix(bads=bads / (tests), tests=tests)
-        print('Bad attributions', bads, 'Total Tests', tests)
+
+                before_afters.append((attribution.mean(), attribution_rewired.mean()))
+                if attribution.mean() > attribution_rewired.mean():
+                    print('attr shapes', attribution_rewired.shape)
+                    print('attr means', attribution.mean(), attribution_rewired.mean())
+                    bads += 1
+                    # print(node_idx)
+
+                tests += 1
+                pbar.set_postfix(bads=bads / (tests), tests=tests)
+            # revert to original
+            for eid in dss.rewiring.node_edits[node_idx]:
+                edge_index_rewired[:, eid] = torch.tensor(dss.rewiring.id_to_edge[eid], dtype=int)
+                rewire_mask[eid] = False
         accs.append((1 - bads / tests))
     return accs
